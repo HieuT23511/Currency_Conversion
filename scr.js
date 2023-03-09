ad555e37eafa4ee75c4d2c3e003813603edaa895
HieuT23511 <hieut23511@gmail.com>
2023-03-09 11:57:14 +0700

    Bai tap chuyen doi tien te

--- scr.js
+++ scr.js
@@ -4,13 +4,13 @@ function Conversion() {
     let To = document.getElementById('To').value;
     let Result;
     if (From === 'USD' && To === "VND") {
-        Result = 'Result: ' + (Amount * 23500) + ' Đồng';
+        Result = 'Kết quả là: ' + (Amount * 23500) + ' Đồng';
     } else if (From === "VND" && To === "USD") {
-        Result = 'Result: ' + (Amount / 23500) + ' $'
+        Result = 'Kết quả là: ' + (Amount / 23500) + ' $'
     } else if (From === "VND") {
-        Result = 'Result: ' + (Amount) + ' Đồng'
+        Result ='Kết quả là: ' + (Amount) + ' Đồng'
     } else {
-        Result = 'Result: ' + (Amount) + ' $'
+        Result ='Kết quả là: ' + (Amount) + ' $'
     }
     document.getElementById("Result").innerHTML= Result;
 }
